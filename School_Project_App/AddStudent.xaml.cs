1a0ca63ff62f97fecc9546ca580420075c765edb
Rahamath22 <rahamath22104@gmail.com>
2024-06-07 12:51:47 +0530

    Student design

---
+++ School_Project_App/AddStudent.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace School_Project_App
+{
+    /// <summary>
+    /// Interaction logic for AddStudent.xaml
+    /// </summary>
+    public partial class AddStudent : Page
+    {
+        public AddStudent()
+        {
+            InitializeComponent();
+        }
+    }
+}
